fc1d738ea7c532f9a52020813340c2ef2b226454
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-03-15 11:21:55 -0400

    Delete folder parse and main and src_old

--- main/quadSolver.h
+++
@@ -1,1 +0,0 @@
-double qsolv(double a, double b, double c, double *x1, double *x2);
--- parse/parse.c
+++
@@ -1,7 +0,0 @@
-#include "parse.h"
-
-int parse(double* a, double* b, double*c, char* str){
-	char* token;
-	token = strtok(line," ");
-	
-}
